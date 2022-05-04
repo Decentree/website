835647b59ae8d25347909c0d2fe82b5b9369708f
fibtx <breberaf@gmail.com>
2022-05-04 14:15:07 +0100

    feat: text update

--- src/constants/solutions.ts
+++ src/constants/solutions.ts
@@ -2,6 +2,6 @@ import { ISolution } from './../interfaces/ISolution';
 export const solutions: ISolution[] = [
     { name: "Mobile Apps", imgPath:"solutions/mobileapps"},
     { name: "Web Apps", imgPath:"solutions/webapps"},
-    { name: "Block Chain", imgPath:"solutions/blockchain"},
-    { name: "DeFi", imgPath:"solutions/defi"}
+    { name: "Solidity Contracts", imgPath:"solutions/blockchain"},
+    { name: "dApps", imgPath:"solutions/defi"}
 ]
