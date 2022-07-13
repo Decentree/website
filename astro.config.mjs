e7fab031596e64ce94ba0a11d5811d7e4e05ff89
Filip Brebera <breberaf@gmail.com>
2022-07-13 21:47:16 +0200

    feat: analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
-import WindiCSS from 'vite-plugin-windicss'
-
+import WindiCSS from 'vite-plugin-windicss';
 import svelte from '@astrojs/svelte';
 import sitemap from '@astrojs/sitemap';
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://decentree.com",
   sitemap: true,
   vite: {
-    plugins: [WindiCSS()],
+    plugins: [WindiCSS()]
   },
-  integrations: [svelte(), sitemap()],
+  integrations: [svelte(), sitemap(), partytown()]
 });
